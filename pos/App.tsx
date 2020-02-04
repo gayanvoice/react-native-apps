8394595b1c530765d70ffdff9923054c6c682a6a
gayankuruppu <example@example.com>
2020-02-04 13:08:59 +0530

    Update App.tsx

--- pos/App.tsx
+++ pos/App.tsx
@@ -1,22 +1,76 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { createAppContainer } from 'react-navigation';
-import { createStackNavigator } from 'react-navigation-stack';
+import React, {Component} from 'react';
 
+import {
+    View,
+    Text,
 
-import HomeScreen from './HomeScreen';
+} from 'react-native';
+import * as Font from "expo-font";
 
-const AppNavigator = createStackNavigator({
-        Home: {
-            screen: HomeScreen,
-        },
-});
+import {createAppContainer} from 'react-navigation';
+import {createStackNavigator} from 'react-navigation-stack';
+
+import HomeScreen from "./HomeScreen";
+
+const AppNavigator = createStackNavigator(
+    {
+        Home: {screen: HomeScreen},
+    },
+    {
+        initialRouteName: 'Home',
+    },
+);
 
 const AppContainer = createAppContainer(AppNavigator);
 
-export default function App() {
-  return (
-      <AppContainer />
-  );
+interface Prop {
+
 }
 
+
+
+interface State  {
+    fontsLoaded: false;
+}
+
+
+
+class App extends Component<Prop, State> {
+    constructor(props: State ) {
+        super(props);
+        this.state = {fontsLoaded: false};
+    }
+
+
+
+    loadFonts() {
+        return Font.loadAsync({
+            "Montserrat-Regular": require("./assets/fonts/Montserrat-Regular.ttf"),
+            "Montserrat-Bold": require("./assets/fonts/Montserrat-Bold.ttf"),
+            "Montserrat-SemiBold": require("./assets/fonts/Montserrat-SemiBold.ttf"),
+            "Montserrat-Medium": require("./assets/fonts/Montserrat-Medium.ttf"),
+            "Montserrat-Light": require("./assets/fonts/Montserrat-Light.ttf")
+        });
+    }
+
+    async componentDidMount() {
+        await this.loadFonts();
+        this.setState({ fontsLoaded: true } as Prop);
+    }
+
+    render() {
+        if(this.state.fontsLoaded){
+            return <AppContainer />;
+        } else {
+            return (
+                <View style={{flex: 1,flexDirection: 'column', alignContent: 'center', justifyContent: 'center', }}>
+                    <Text style={{padding: 24}}>Loading</Text>
+                </View>
+            )
+        }
+
+
+    }
+}
+
+export default App;
