29d4df4041af7e820e99f8dec4a764d5b5915e28
gayankuruppu <example@example.com>
2020-02-04 12:14:52 +0530

    Update POS

--- pos/App.tsx
+++ pos/App.tsx
@@ -1,19 +1,22 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+
+
+import HomeScreen from './HomeScreen';
+
+const AppNavigator = createStackNavigator({
+        Home: {
+            screen: HomeScreen,
+        },
+});
+
+const AppContainer = createAppContainer(AppNavigator);
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-    </View>
+      <AppContainer />
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
