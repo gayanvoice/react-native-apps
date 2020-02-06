df04fb76d001eacab395ab3ab3ae2403911afaf1
gayankuruppu <example@example.com>
2020-02-06 09:27:24 +0530

    Update App.tsx

--- pos/App.tsx
+++ pos/App.tsx
@@ -2,52 +2,147 @@ import React, {Component} from 'react';
 
 import {
     View,
-    Text,
+    Text, TouchableOpacity, Image, StyleSheet, Dimensions,
 
 } from 'react-native';
 import * as Font from "expo-font";
 
-import {createAppContainer} from 'react-navigation';
-import {createStackNavigator} from 'react-navigation-stack';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+
 
 import HomeScreen from "./screens/HomeScreen";
 import ContactScreen from "./screens/ContactScreen";
 import AboutScreen from "./screens/AboutScreen";
+import SupportScreen from "./screens/SupportScreen";
+import Colors from "./constants/Colors";
+import Sizes from "./constants/Sizes";
 
-export enum ROUTES {
-    MainHome = "MainHome",
-    MainContact = "MainContact",
-    MainAbout = "MainAbout"
+const Stack = createStackNavigator();
+
+const { width } = Dimensions.get('window');
+
+const styles = StyleSheet.create({
+
+    flex: {
+        flex: 0,
+    },
+
+    column: {
+        flexDirection: 'column'
+    },
+
+    row: {
+        flexDirection: 'row'
+    },
+
+    header: {
+        backgroundColor: Colors.gray4,
+        paddingHorizontal: Sizes.padding,
+        paddingTop: Sizes.padding,
+        paddingBottom: Sizes.padding * 0.66,
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        fontFamily: 'Montserrat-Regular'
+    },
+    avatar: {
+        width: Sizes.padding,
+        height: Sizes.padding,
+        borderRadius: Sizes.padding / 2,
+    },
+
+    bills: {
+        flex: 1,
+        justifyContent: 'space-between',
+        paddingBottom: 30,
+        fontFamily: 'Montserrat-Regular'
+    },
+
+    order_view: {
+        width: (width - (Sizes.padding * 2)) / 2,
+        marginHorizontal: Sizes.padding / 4,
+        marginTop: Sizes.padding / 2,
+        marginBottom: Sizes.padding / 2,
+        backgroundColor: Colors.white,
+        borderRadius: Sizes.radius,
+        fontFamily: 'Montserrat-Regular'
+    },
+
+    order_options: {
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        left: 0,
+        right: 0,
+        fontSize: Sizes.font,
+        fontFamily: 'Montserrat-Regular'
+    },
+
+    order_id: {
+        fontSize: Sizes.font,
+        color: Colors.black,
+        fontFamily: 'Montserrat-Bold'
+    },
+
+    shadow: {
+        shadowColor: Colors.black,
+        shadowOffset: {
+            width: 0,
+            height: 3,
+        },
+        shadowOpacity: 0.05,
+        shadowRadius: 10,
+        elevation: 2,
+    },
+
+    title: {
+        fontSize: Sizes.font * 2,
+        fontFamily: 'Montserrat-Bold',
+    },
+
+    contentTitle: {
+        fontSize: Sizes.font,
+        fontFamily: 'Montserrat-Bold',
+    },
+
+    contentRow: {
+        justifyContent: 'space-between',
+        alignItems: 'flex-end',
+        paddingStart: Sizes.padding,
+        paddingEnd: Sizes.padding,
+        fontFamily: 'Montserrat-Regular'
+    },
+
+});
+
+
+function MyStack() {
+    return (
+        <Stack.Navigator initialRouteName={"Home"}>
+            <Stack.Screen name="Home" component={HomeScreen} options={{ title: 'My home',  header: ({navigation}) => (
+                    <>
+                        <View style={[styles.flex, styles.row, styles.header,]}>
+
+                            <View>
+                                <Text style={[styles.title]}>POS</Text>
+                            </View>
+                            <TouchableOpacity activeOpacity={0.5} onPress={() => {navigation.navigate('Contact')}}>
+                                <Image style={styles.avatar} source={{uri: 'https://avatars1.githubusercontent.com/u/30500175?s=48&v=4'}} />
+                            </TouchableOpacity>
+                        </View>
+
+                    </>
+                ), }} />
+            <Stack.Screen name="Contact" component={ContactScreen} />
+            <Stack.Screen name="About" component={AboutScreen} />
+            <Stack.Screen name="Support" component={SupportScreen} />
+        </Stack.Navigator>
+    );
 }
 
 
-const AppNavigator = createStackNavigator(
-    {
-        [ROUTES.MainHome]: { screen:  HomeScreen },
-        [ROUTES.MainContact]: { screen: ContactScreen },
-        [ROUTES.MainAbout]: { screen: AboutScreen },
-    },
-    {
-        initialRouteName: ROUTES.MainAbout,
-    },
-);
-
-const AppContainer = createAppContainer(AppNavigator);
-
-interface Prop {}
-interface State  {
-    fontsLoaded: false;
-}
-
-
-
-export default class App extends Component<Prop, State> {
-    constructor(props: State ) {
-        super(props);
-        this.state = {fontsLoaded: false};
-    }
-
-
+export default class App extends Component {
+    state = {fontsLoaded: false};
 
     loadFonts() {
         return Font.loadAsync({
@@ -61,12 +156,14 @@ export default class App extends Component<Prop, State> {
 
     async componentDidMount() {
         await this.loadFonts();
-        this.setState({ fontsLoaded: true } as Prop);
+        this.setState({ fontsLoaded: true });
     }
 
     render() {
         if(this.state.fontsLoaded){
-            return <AppContainer />;
+            return ( <NavigationContainer>
+                <MyStack />
+            </NavigationContainer>);
         } else {
             return (
                 <View style={{flex: 1,flexDirection: 'column', alignContent: 'center', justifyContent: 'center', }}>
